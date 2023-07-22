348ebd4bde5c416922a5a20e4f72af16b0fbf97c
dreina99 <devinreina99@gmail.com>
2023-07-22 12:54:14 -0700

    Add basic button capture

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let buttonLabels = [[7, 4, 1, 'C'],
                     [8, 5, 2, 0],
                     [9, 6, 3, '.'],
                     ['+/-', 'x', '-', '+'],
-                    ['->', '÷', '=']]
+                    ['\u2192', '÷', '=']]
+let savedEq = document.querySelector('.save-eq')
+let inputEq = document.querySelector('.input')
 
 for (let i = 0; i < 5; i++) {
     // create column for buttons
@@ -19,12 +21,22 @@ for (let i = 0; i < 5; i++) {
         if (i == 4 && j == 2) button.classList.add('equals')
         else button.classList.add('button')
 
-        // create labels
+        // create labels    
         let label = document.createElement('span')
+        label.classList.add('button-label')
         label.innerHTML = buttonLabels[i][j]
         button.appendChild(label)
-        
+
         column.appendChild(button)
     }
     buttonContainer.appendChild(column)
 }
+
+// capture event for buttons on page
+let btns = document.querySelectorAll('.button')
+btns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        let btnLabel = btn.children[0].innerHTML
+        console.log(btnLabel)
+    })
+})
