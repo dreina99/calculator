5fda98995fa3f92995070d38e1f593343a2829ef
dreina99 <devinreina99@gmail.com>
2023-07-22 12:27:59 -0700

    Create and label buttons

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 let buttonContainer = document.querySelector('#button-container')
+let buttonLabels = [[7, 4, 1, 'C'], 
+                    [8, 5, 2, 0],
+                    [9, 6, 3, '.'],
+                    ['+/-', 'x', '-', '+'],
+                    ['->', '÷', '=']]
 
 for (let i = 0; i < 5; i++) {
+    // create column for buttons
     let column = document.createElement('div')
     column.classList.add('col')
-    for (let j = 0; j < 4; j++) {
-        if(i == 4 && j == 3) break
-        let button = document.createElement('button')
 
+    for (let j = 0; j < 4; j++) {
+        // break for equals button
+        if(i == 4 && j == 3) break
+        
+        // create button
+        let button = document.createElement('button')
         if (i == 4 && j == 2) button.classList.add('equals')
         else button.classList.add('button')
 
+        // create labels
+        let label = document.createElement('span')
+        label.innerHTML = buttonLabels[i][j]
+        button.appendChild(label)
+        
         column.appendChild(button)
     }
     buttonContainer.appendChild(column)
